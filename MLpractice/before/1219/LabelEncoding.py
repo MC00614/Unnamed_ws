ccf762c7eaeb6a02256630a1454ab52bf215a160
MC00614 <12190588@inha.edu>
2023-01-05 12:33:50 +0900

    redesign

--- MLpractice/before/1219/LabelEncoding.py
+++
@@ -1,9 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-
-items = ['TV', '냉장고', '전자레인지', '컴퓨터', '선풍기', '선풍기', '믹서', '믹서']
-
-encoder = LabelEncoder()
-encoder.fit(items)
-labels = encoder.transform(items)
-print(encoder.classes_)
-
